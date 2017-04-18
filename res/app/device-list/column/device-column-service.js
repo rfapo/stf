334435f55b26c612cd8d3e82e22fda6b0432830f
Rodrigo Paiva <rodrigo.paiva@onisolucoes.net>
2017-04-18 08:32:35 -0300

    Update device-column-service.js
    Reset last commit.

--- res/app/device-list/column/device-column-service.js
+++ res/app/device-list/column/device-column-service.js
@@ -255,7 +255,7 @@ module.exports = function DeviceColumnService($filter, gettext) {
   , provider: TextCell({
       title: gettext('Location')
     , value: function(device) {
-        return device.url || ''
+        return device.provider ? device.provider.name : ''
       }
     })
   , notes: DeviceNoteCell({
